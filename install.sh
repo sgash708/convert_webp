f0b62d3063c1fc519e9a6ba9fbe314b8abf6a1b5
sgash708 <sugash7081023@gmail.com>
2021-08-25 16:06:18 +0900

    inotifyに対するコメント追加

--- install.sh
+++ install.sh
@@ -1,18 +1,11 @@
 #!/bin/bash
 
-# inotify-tools
 # REF: https://www.server-world.info/query?os=CentOS_6&p=inotify
 sudo su
 
-# webp設定
+# webp
 yum install libwebp-tools
-mkdir /var/www/html/webp
-wget -c "https://upload.wikimedia.org/wikipedia/commons/2/24/Junonia_orithya-Thekkady-2016-12-03-001.jpg?download" -O /var/www/html/webp/image1.jpg
-wget -c "https://upload.wikimedia.org/wikipedia/commons/5/54/Mycalesis_junonia-Thekkady.jpg" -O /var/www/html/webp/image2.jpg
-wget -c "https://cdn.pixabay.com/photo/2017/07/18/15/39/dental-care-2516133_640.png" -O /var/www/html/webp/logo.png
 
-# inotify設定
+# inotify
 yum install epel-release
 yum install inotify-tools
-touch /etc/inotifywait.conf/
-echo "LOGFILE=/var/log/inotify.log\nMONITOR=/etc\nEVENT=create,delete,modify,move" >> /etc/inotifywait.conf
--- watch_webp.sh
+++ watch_webp.sh
@@ -8,6 +8,15 @@ JPEG_CWEBP_OPTS="-q 75 -m 4"
 # PNG向け可逆cwebpオプション
 PNG_CWEBP_OPTS="-lossless"
 
+<< COMMENT
+inotifywait: This command watches for changes to a certain directory.
+-q: This option will tell inotifywait to be quiet and not produce a lot of output.
+-m: This option will tell inotifywait to run indefinitely and not exit after receiving a single event.
+-r: This option will set up watchers recursively, watching a specified directory and all its sub-directories.
+--format: This option tells inotifywait to monitor changes using the event name followed by the file path. The events we want to monitor are close_write (triggered when a file is created and completely written to the disk), moved_from and moved_to (triggered when a file is moved), and delete (triggered when a file is deleted).
+$1: This positional parameter holds the path of the changed files.
+COMMENT
+
 # watch for any created, moved, or deleted image files
 inotifywait -q -m -r --format '%e %w%f' -e close_write -e moved_from -e moved_to -e delete $1 | grep -i -E '\.(jpe?g|png)$' --line-buffered \
 | while read operation path; do
